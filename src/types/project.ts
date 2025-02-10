bde1e98250e12b4789eb63a22cbea3814372a361
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-02-10 23:48:46 +0700

    Refactor project and participant types to include revision history and enhance user role management

--- src/types/project.ts
+++ src/types/project.ts
@@ -2,39 +2,72 @@ export interface User {
     _id: string;
     username: string;
     email: string;
-    avatar_url: string;
-    role: 'leader' | 'staff';
-    status: 'online' | 'offline';
-    date_of_birth: Date;
-    verify: string;
-    last_login_time: Date;
+    avatar_url: string | "";
+    // role: 'leader' | 'staff';
+    // status: 'online' | 'offline';
+    // date_of_birth: Date;
+    // verify: string;
+    // last_login_time: Date;
 }
 
 export interface Participant {
     _id: string;
+    role: string;
+    status: string;
+    joined_at: Date;
     user: User;
 }
 
-export interface Task {
-    id: string;
-    title: string;
-    status: 'completed' | 'in_progress' | 'pending';
+export interface Revision {
+    modifiedAt: Date;
+    modifiedBy: {
+        _id: string;
+        username: string;
+        email: string;
+        avatar_url: string | "";
+    };
+    changes: Map<string, {
+        from: any;
+        to: any;
+    }>;
+    changeDescription: string;
 }
 
-export interface Project {
+export interface ResParticipant {
+    _id: string;
+    project_id: string;
+    user_id: string;
+    role: string;
+    status: string;
+    joined_at: Date;
+    username: string;
+    email: string;
+    avatar_url: string | "";
+}
+
+export interface ResProject {
     _id: string;
     title: string;
     description: string;
     key: string;
-    hasBeenModified: boolean;
-    participants: Participant[];
-    tasks: Task[];
+    creator: User;
+    participants: ResParticipant[];
     created_at: Date;
     deleted: boolean;
+    hasBeenModified: boolean;
+    revisionHistory: Revision[];
+    leader: ResParticipant;
 }
 
 export interface NewProject {
     title: string;
     description: string;
     key: string;
+    creator: string;
+    participants?: string[];
+    created_at: Date;
+}
+
+export interface Project extends Omit<ResProject, 'participants'> {
+    participants: ResParticipant[];
 }
