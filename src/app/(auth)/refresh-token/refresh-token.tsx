9874dd097c15c54cc15f235097c7565618b5650a
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-02-12 23:48:20 +0700

    Add refresh token component to handle token validation and redirection

---
+++ src/app/(auth)/refresh-token/refresh-token.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { checkAndRefreshToken, getRefreshTokenFromLocalStorage } from '@/lib/utils'
+import { useRouter } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+
+export default function RefreshToken() {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const refreshTokenFromUrl = searchParams.get('refresh_token')
+  const redirectPathname = searchParams.get('redirect')
+  useEffect(() => {
+    if (refreshTokenFromUrl && refreshTokenFromUrl === getRefreshTokenFromLocalStorage()) {
+      checkAndRefreshToken({
+        onSuccess: () => {
+          router.push(redirectPathname || '/')
+        }
+      })
+    } else {
+      router.push('/')
+    }
+  }, [router, refreshTokenFromUrl, redirectPathname])
+  return <div>Refresh token....</div>
+}
