2e7d3dbb18d0a1d99ff8d7b6494834e7dc6577ed
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-02-12 23:50:28 +0700

    Add registration route to handle user sign-up and token storage

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,45 @@
+import authApiRequest from '@/api-requests/auth'
+import { RegisterBodyType } from '@/schema-validations/auth.schema'
+import { cookies } from 'next/headers'
+import jwt from 'jsonwebtoken'
+import { HttpError } from '@/lib/http'
+export async function POST(request: Request) {
+  const body = (await request.json()) as RegisterBodyType
+  const cookieStore = await cookies()
+  try {
+    const { payload } = await authApiRequest.sRegister(body)
+    const { access_token, refresh_token } = payload.metadata
+    const decodedAccessToken = jwt.decode(access_token) as { exp: number }
+    const decodedRefreshToken = jwt.decode(refresh_token) as { exp: number }
+    cookieStore.set('access_token', access_token, {
+      path: '/',
+      httpOnly: true,
+      sameSite: 'lax',
+      secure: true,
+      expires: decodedAccessToken.exp * 1000
+    })
+    cookieStore.set('refresh_token', refresh_token, {
+      path: '/',
+      httpOnly: true,
+      sameSite: 'lax',
+      secure: true,
+      expires: decodedRefreshToken.exp * 1000
+    })
+    return Response.json(payload)
+  } catch (error) {
+    if (error instanceof HttpError) {
+      return Response.json(error.payload, {
+        status: error.status
+      })
+    } else {
+      return Response.json(
+        {
+          message: 'Có lỗi xảy ra'
+        },
+        {
+          status: 500
+        }
+      )
+    }
+  }
+}
