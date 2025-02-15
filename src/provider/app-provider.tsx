ecb83d5ab7ff20e8e1423a3b377290711e6ffdc0
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-02-15 14:08:02 +0700

    Enhance app provider with role management and socket handling improvements

--- src/provider/app-provider.tsx
+++ src/provider/app-provider.tsx
@@ -1,21 +1,32 @@
 'use client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-// import RefreshToken from '@/components/refresh-token'
 import type { Socket } from 'socket.io-client'
 import { useEffect, useRef } from 'react'
 import { create } from 'zustand'
-import { generateSocketInstace, getAccessTokenFromLocalStorage } from '@/lib/utils'
+import {
+  decodeToken,
+  generateSocketInstace,
+  getAccessTokenFromLocalStorage,
+  removeTokensFromLocalStorage
+} from '@/lib/utils'
+import RefreshToken from '@/components/refresh-token'
+import { RoleType } from '@/types/jwt.type'
+import ListenLogoutSocket from '@/components/listen-logout-socket'
 
-const queryClient = new QueryClient({
+export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false
+      refetchOnWindowFocus: false,
+      staleTime: 0,
+      retry: 1
     }
   }
 })
 
 type AppStoreType = {
   isAuth: boolean
+  role: RoleType | undefined
+  setRole: (role?: RoleType) => void
   socket: Socket | undefined
   setSocket: (socket?: Socket | undefined) => void
   disconnectSocket: () => void
@@ -23,6 +34,13 @@ type AppStoreType = {
 
 export const useAppStore = create<AppStoreType>((set) => ({
   isAuth: false,
+  role: undefined as RoleType | undefined,
+  setRole: (role?: RoleType | undefined) => {
+    set({ role, isAuth: Boolean(role) })
+    if (!role) {
+      removeTokensFromLocalStorage()
+    }
+  },
   socket: undefined as Socket | undefined,
   setSocket: (socket?: Socket | undefined) => set({ socket }),
   disconnectSocket: () =>
@@ -33,25 +51,27 @@ export const useAppStore = create<AppStoreType>((set) => ({
 }))
 
 export default function AppProvider({ children }: { children: React.ReactNode }) {
+  const setRole = useAppStore((state) => state.setRole)
   const setSocket = useAppStore((state) => state.setSocket)
-
-  const count = useRef(0) // Đếm số lần render của component
+  const count = useRef(0)
 
   useEffect(() => {
     if (count.current === 0) {
       const access_token = getAccessTokenFromLocalStorage()
       if (access_token) {
+        const role = decodeToken(access_token).role
+        setRole(role)
         setSocket(generateSocketInstace(access_token))
       }
       count.current++
     }
-  }, [setSocket])
+  }, [setRole, setSocket])
 
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      {/* <RefreshToken /> */}
-      {/* <ListenLogoutSocket /> */}
+      <RefreshToken />
+      <ListenLogoutSocket />
     </QueryClientProvider>
   )
 }
