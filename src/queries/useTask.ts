7c3653d381fc3baf524c53763a99d58af86ccf82
Ken Ny <kennynguyen1631984@gmail.com>
2025-02-17 10:23:26 +0700

    Add task-related query hooks for project tasks and subtasks

--- src/queries/useTask.ts
+++ src/queries/useTask.ts
@@ -1,9 +1,10 @@
 import projectApiRequest from "@/api-requests/project"
 import taskApiRequest from "@/api-requests/task"
 import { UpdateTaskBodyType } from "@/schema-validations/task.schema"
-import { useInfiniteQuery, useMutation, useQueryClient } from "@tanstack/react-query"
+import { NewTask } from "@/types/task"
+import { useInfiniteQuery, useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 
-export const useGetTasksQueryOfProject = (projectId: string) => {
+export const useGetTasksQueryOfProject = (projectId: string,) => {
     return useInfiniteQuery({
         queryKey: ['tasks', projectId],
         queryFn: async ({ pageParam = 1 }) => projectApiRequest.sGetTasksOfProject(projectId, pageParam),
@@ -19,12 +20,57 @@ export const useGetTasksQueryOfProject = (projectId: string) => {
     })
 }
 
+export const useGetAllTasksOfProject = (projectId: string) => {
+    return useQuery({
+        queryKey: ['all-tasks', projectId],
+        queryFn: async () => {
+            // Gọi API với limit lớn hoặc parameter để lấy tất cả
+            const response = await projectApiRequest.sGetAllTasksOfProject(projectId)
+            return response
+        },
+        enabled: !!projectId,
+    })
+}
+
 export const useUpdateTaskMutation = () => {
     const queryClient = useQueryClient()
     return useMutation({
         mutationFn: (params: { projectId: string, taskId: string; body: UpdateTaskBodyType }) => taskApiRequest.sUpdateTask(params.projectId, params.taskId, params.body),
-        onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ['tasks'] })
+        onSuccess: (_, variables) => {
+            queryClient.invalidateQueries({ 
+                queryKey: ['tasks', variables.projectId]
+            })
         }
     })
 }
+
+export const useCreateTaskMutation = () => {
+    const queryClient = useQueryClient()
+    
+    return useMutation({
+        mutationFn: (params: { projectId: string, body: NewTask }) => {
+            return taskApiRequest.sCreateTask(params.projectId, params.body)
+        },
+        onSuccess: (_, variables) => {
+            queryClient.invalidateQueries({ 
+                queryKey: ['tasks', variables.projectId]
+            })
+
+            queryClient.invalidateQueries({
+                queryKey: ['all-tasks', variables.projectId]
+            })
+        },
+        onError: (error) => {
+            console.error('Failed to create task:', error);
+        }
+    })
+}
+
+
+export const useGetSubTasksOfTask = (projectId: string, taskId: string) => {
+    return useQuery({
+        queryKey: ['sub-tasks', projectId, taskId],
+        queryFn: () => taskApiRequest.sGetSubTasksOfTask(projectId, taskId)
+    })
+}
+
