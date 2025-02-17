858adf93f8ec1679d656cfc7b3e314e3ac923cfb
Ken Ny <kennynguyen1631984@gmail.com>
2025-02-17 10:21:17 +0700

    Add API methods for task creation and subtask retrieval

--- src/api-requests/task.ts
+++ src/api-requests/task.ts
@@ -1,5 +1,6 @@
 import http from '@/lib/http'
-import { TaskResType, UpdateTaskBodyType } from '@/schema-validations/task.schema'
+import { SubTaskResType, TaskResType, UpdateTaskBodyType } from '@/schema-validations/task.schema'
+import { NewTask } from '@/types/task'
 
 const taskApiRequest = {
     // sGetTasksOfProject: (projectId: string, page: number) =>
@@ -8,5 +9,13 @@ const taskApiRequest = {
     sUpdateTask: (projectId: string, taskId: string, body: UpdateTaskBodyType) =>
         http.patch<TaskResType>(`/projects/${projectId}/tasks/${taskId}`, body),
 
+    sCreateTask: (projectId: string, body: NewTask) =>
+    {
+        console.log('body', body)
+        return http.post<TaskResType>(`/projects/${projectId}/tasks`, body)
+    },
+
+    sGetSubTasksOfTask: (projectId: string, taskId: string) =>
+        http.get<SubTaskResType>(`/projects/${projectId}/tasks/${taskId}/subtasks`)
 }
 export default taskApiRequest
