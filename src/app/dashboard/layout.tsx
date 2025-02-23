914d3808090bad6d77073eda70fba2369961882e
Ken Ny <kennynguyen1631984@gmail.com>
2025-02-24 06:00:33 +0700

    Refactor layout components to remove unnecessary ThemeProvider wrapping and adjust theme properties

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,16 +4,11 @@ import { ThemeProvider } from '@/provider/theme-provider'
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute='class' defaultTheme='light' enableSystem disableTransitionOnChange>
-      {/* <div className='lg:hidden flex flex-col items-center justify-center h-screen'>
-        <Badge>Mobile currently unavailable</Badge>
-        <div>Please open this page on desktop</div>
-      </div> */}
-      {/* <div className='hidden lg:flex h-screen w-full'> */}
+    <div >
       <div className='lg:flex h-screen w-full'>
         <SidebarDashboard />
         <div className='w-full ml-[55px] scrollbar-default dark:scrollbar-dark'>{children}</div>
       </div>
-    </ThemeProvider>
+    </div>
   )
 }
