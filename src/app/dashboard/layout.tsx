1633c17abca3181c638c07217eae4987ea4db52f
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-03-29 22:37:19 +0700

    Add SocketStatus component to DashboardLayout for real-time connection status display

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,13 +1,15 @@
 import SidebarDashboard from '@/app/dashboard/sidebar-dashboard'
 import { Badge } from '@/components/ui/badge'
 import { ThemeProvider } from '@/provider/theme-provider'
+import { SocketStatus } from '@/components/ui/socket-status'
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div >
+    <div>
       <div className='lg:flex h-screen w-full'>
         <SidebarDashboard />
         <div className='w-full ml-[55px] scrollbar-default dark:scrollbar-dark'>{children}</div>
+        <SocketStatus />
       </div>
     </div>
   )
